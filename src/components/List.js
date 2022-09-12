8990700bd16e50db1d40295e771f184d2b336c62
Piyush <piyushbansal941@gmail.com>
2022-09-13 00:18:26 +0530

    worked on multiple changes

--- src/components/List.js
+++ src/components/List.js
@@ -17,20 +17,20 @@ function List() {
                     success: function(data) {
                         
                         setdetails(details => details.filter((x)=>{
-                            return x.id != id;
+                            return x.id !== id;
                         }));
                         // youcan also this as a challenge without page reload data removal...
                         
                         alert("Details removed successfully");
                         console.log(data);
-                    }.bind(this),
+                    },
                     // Fail..
                     error: function(error) {
                         
                         console.log(error);
                         
                         alert("Sorry some technical issue.");
-                    }.bind(this)
+                    }
                 });
             }
         }
@@ -39,12 +39,21 @@ function List() {
             window.location.reload();
         }
     }
-
+    function editDetails(id){
+       
+        var name=document.getElementsByClassName('name'+id)[0].innerText;
+        var nickName=document.getElementsByClassName('nickName'+id)[0].innerText;
+        var schoolName=document.getElementsByClassName('schoolName'+id)[0].innerText;
+        
+        $('#editFormName').val(name);
+        $('#editFormNickName').val(nickName);
+        $('#editFormSchoolName').val(schoolName);
+        $("#myEditModalButton").click();
+    }
+    
     function showFullDetails(text){
-        // $('#text').text(text);
-        // $("#modal67").modal('show');
-        // console.log( $('#text').text(),$("#fullDetails").html());
-        alert(text);
+        $('#text').text(text);
+        $("#myModalButton").click();
     }
     const [details, setdetails] = useState([]);
     
@@ -86,11 +95,14 @@ function List() {
                         x.name ?
                         <tr key={i}>
                             <th scope="row">{i}</th>
-                            <td className='manage_overflow text-truncate' onClick={() => showFullDetails(x.name)}>{x.name}</td>
-                            <td className='manage_overflow text-truncate' onClick={() => showFullDetails(x.nick_name)}>{x.nick_name}</td>
-                            <td className='manage_overflow text-truncate' onClick={() => showFullDetails(x.school_name)}>{x.school_name}</td>
+                            <td className={'manage_overflow text-truncate name'+x.id} onClick={() => showFullDetails(x.name)}>{x.name}</td>
+                            <td className={'manage_overflow text-truncate nickName'+x.id} onClick={() => showFullDetails(x.nick_name)}>{x.nick_name}</td>
+                            <td className={'manage_overflow text-truncate schoolName'+x.id} onClick={() => showFullDetails(x.school_name)}>{x.school_name}</td>
                             <td>{x.created_at}</td>
                             <td>
+                                <button className='btn btn-outline-primary' style={{"margin":"3px"}} id={x.id} onClick={() => editDetails(x.id)}>
+                                    Edit
+                                </button>
                                 <button className='btn btn-outline-danger' id={x.id} onClick={() => removeDetails(x.id)}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
                                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
@@ -109,21 +121,62 @@ function List() {
 
 
             {/* Modal */}
+        
+<button type="button" id="myModalButton" hidden className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+  Launch modal
+</button>
+     
+<button type="button" id="myEditModalButton" hidden className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myEditModal">
+  Launch modal
+</button>
 
-            <div className="modal fade" tabIndex="-1" id='modal67'>
+
+            <div className="modal fade" tabIndex="-1" id='myModal'>
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title"></h5>
+                        <h5 className="modal-title"> </h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
                         <p id='text'>text goes here.</p>
                     </div>
-                    <div className="modal-footer">
-                        {/* <button type="button" className="btn btn-primary">Save changes</button> */}
+                    {/* <div className="modal-footer">
+                        <button type="button" className="btn btn-primary">Save changes</button> 
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div> */}
                     </div>
+                </div>
+            </div>
+
+            <div className="modal fade" tabIndex="-1" id='myEditModal'>
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Edit Details</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="#">
+                        <div className="modal-body text-start">
+                            <input type="hidden" name='row_id' readOnly/>
+                            <div className="mb-3">
+                                <label htmlFor="name" className='text-primary'><b>Name</b><span className='text-danger'> *</span></label>
+                                <input className='form-control' type="text" name='myName' id='editFormName' />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="nickName" className='text-primary'><b>Nick Name</b><span className='text-danger'> *</span></label>
+                                <input className='form-control' type="text" name='nickName' id='editFormNickName' />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="schoolName" className='text-primary'><b>School Name</b><span className='text-danger'> *</span></label>
+                                <input className='form-control' type="text" name='schoolName' id='editFormSchoolName' />
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-primary">coming soon</button>
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </form>
                     </div>
                 </div>
             </div>
